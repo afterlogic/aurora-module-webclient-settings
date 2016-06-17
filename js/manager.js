c3d164dcfce614fe923553910b11b910ec3aeb9c
nadine-shi <nadine@afterlogic.com>
2016-06-17 12:44:39 +0300

    AdminPanel module

--- js/manager.js
+++ js/manager.js
@@ -1,47 +1,63 @@
 'use strict';
 
-module.exports = function (oAppData) {
+module.exports = function (oAppData, iUserRole, bPublic) {
 	var
-		Settings = require('modules/%ModuleName%/js/Settings.js'),
-		oSettings = oAppData['%ModuleName%'] || {}
+		bAdminUser = iUserRole === Enums.UserRole.SuperAdmin,
+		bPowerUser = iUserRole === Enums.UserRole.PowerUser,
+		bRegisteredUser = iUserRole === Enums.UserRole.RegisteredUser
 	;
 	
-	Settings.init(oSettings);
-	
-	return {
-		isAvailable: function (iUserRole, bPublic) {
-			return !bPublic && iUserRole === Enums.UserRole.PowerUser || iUserRole === Enums.UserRole.RegisteredUser;
-		},
-		getScreens: function () {
-			var oScreens = {};
-			oScreens[Settings.HashModuleName] = function () {
-				return require('modules/%ModuleName%/js/views/SettingsView.js');
-			};
-			return oScreens;
-		},
-		getHeaderItem: function () {
-			var
-				TextUtils = require('modules/CoreClient/js/utils/Text.js'),
-				CHeaderItemView = require('modules/CoreClient/js/views/CHeaderItemView.js')
-			;
+	if (bAdminUser || bPowerUser || bRegisteredUser)
+	{
+		var
+			Settings = require('modules/%ModuleName%/js/Settings.js'),
+			oSettings = oAppData['%ModuleName%'] || {}
+		;
+
+		Settings.init(oSettings);
+		
+		if (bAdminUser)
+		{
 			return {
-				item: new CHeaderItemView(TextUtils.i18n('CORECLIENT/HEADING_SETTINGS_TABNAME')),
-				name: Settings.HashModuleName
+				getAbstractSettingsFormViewClass: function () {
+					return require('modules/%ModuleName%/js/views/CAbstractSettingsFormView.js');
+				}
 			};
-		},
-		registerSettingsTab: function (fGetTabView, oTabName, oTabTitle) {
-			var SettingsView = require('modules/%ModuleName%/js/views/SettingsView.js');
-			SettingsView.registerTab(fGetTabView, oTabName, oTabTitle);
-		},
-		getSettingsUtils: function () {
-			return require('modules/%ModuleName%/js/utils/Settings.js');
-		},
-		getAbstractSettingsFormViewClass: function () {
-			return require('modules/%ModuleName%/js/views/CAbstractSettingsFormView.js');
-		},
-		setAddHash: function (aAddHash) {
-			var SettingsView = require('modules/%ModuleName%/js/views/SettingsView.js');
-			SettingsView.setAddHash(aAddHash);
 		}
-	};
+		else if (bPowerUser || bRegisteredUser)
+		{
+			return {
+				getScreens: function () {
+					var oScreens = {};
+					oScreens[Settings.HashModuleName] = function () {
+						return require('modules/%ModuleName%/js/views/SettingsView.js');
+					};
+					return oScreens;
+				},
+				getHeaderItem: function () {
+					var
+						TextUtils = require('modules/CoreClient/js/utils/Text.js'),
+						CHeaderItemView = require('modules/CoreClient/js/views/CHeaderItemView.js')
+					;
+					return {
+						item: new CHeaderItemView(TextUtils.i18n('CORECLIENT/HEADING_SETTINGS_TABNAME')),
+						name: Settings.HashModuleName
+					};
+				},
+				registerSettingsTab: function (fGetTabView, oTabName, oTabTitle) {
+					var SettingsView = require('modules/%ModuleName%/js/views/SettingsView.js');
+					SettingsView.registerTab(fGetTabView, oTabName, oTabTitle);
+				},
+				getAbstractSettingsFormViewClass: function () {
+					return require('modules/%ModuleName%/js/views/CAbstractSettingsFormView.js');
+				},
+				setAddHash: function (aAddHash) {
+					var SettingsView = require('modules/%ModuleName%/js/views/SettingsView.js');
+					SettingsView.setAddHash(aAddHash);
+				}
+			};
+		}
+	}
+	
+	return null;
 };
